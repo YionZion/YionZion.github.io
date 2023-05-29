89463821017eebe420852cd2e4de617483f93534
YionZion <YionZion@pixiv.net>
2023-05-29 22:13:01 +0800

    优化

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -17,30 +17,30 @@ export class MainComponent {
 	};
 
 	products = [
-		{url: 'https://afdian.net/order/create?plan_id=b0ed0668e5b911eda64852540025c377&product_type=2', name: '染绿11-15合集'},
+		{url: 'https://afdian.net/order/create?plan_id=b0ed0668e5b911eda64852540025c377&product_type=2', name: '染11-15合集'},
 		{
 			url: 'https://afdian.net/order/create?product_type=1&plan_id=0ed84074e5b711edb77c52540025c377&sku=%5B%7B%22sku_id%22%3A%220edb2082e5b711edb33152540025c377%22,%22count%22%3A1%7D%5D',
-			name: '染绿11'
+			name: '染11'
 		},
 		{
 			url: 'https://afdian.net/order/create?product_type=1&plan_id=5b647304e5b711edb18d52540025c377&sku=%5B%7B%22sku_id%22%3A%225b6768d4e5b711edbf5852540025c377%22,%22count%22%3A1%7D%5D',
-			name: '染绿12'
+			name: '染12'
 		},
 		{
 			url: 'https://afdian.net/order/create?product_type=1&plan_id=822360aee5b711ed817a52540025c377&sku=%5B%7B%22sku_id%22%3A%2282262690e5b711edbb5b52540025c377%22,%22count%22%3A1%7D%5D',
-			name: '染绿13'
+			name: '染13'
 		},
 		{
 			url: 'https://afdian.net/order/create?product_type=1&plan_id=ae1a848ae5b711edbfd852540025c377&sku=%5B%7B%22sku_id%22%3A%22ae1d5c14e5b711ed8ce452540025c377%22,%22count%22%3A1%7D%5D',
-			name: '染绿14'
+			name: '染14'
 		},
 		{
 			url: 'https://afdian.net/order/create?product_type=1&plan_id=df1ea2d2e5b711edbede52540025c377&sku=%5B%7B%22sku_id%22%3A%22df218c86e5b711ed98d752540025c377%22,%22count%22%3A1%7D%5D',
-			name: '染绿15'
+			name: '染15'
 		},
 		{
 			url: 'https://afdian.net/order/create?product_type=1&plan_id=8647c972f5ed11ed98df5254001e7c00&sku=%5B%7B%22sku_id%22%3A%22864aea1cf5ed11edaa715254001e7c00%22,%22count%22%3A1%7D%5D',
-			name: '染绿16'
+			name: '染16'
 		},
 	];
 
